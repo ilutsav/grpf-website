6f498d52cea85117b1af056dba609ab6f56383c1
ilutsav <ilutsav@gmail.com>
2025-09-05 16:15:11 +0545

    trying live

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/family-website/',
+  base: '/grpf-website/',
   build: {
     outDir: 'dist'
   }
